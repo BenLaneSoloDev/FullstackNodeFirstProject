56aa1ccb3d4bba8a9cc3735214a8c2c909f3403b
Ben-Lane <benjaminkieranlane@gmail.com>
2026-06-25 12:34:34 +0100

    Created REST API Notes

--- notes.js
+++ notes.js
@@ -21,7 +21,48 @@ const fs = require("fs");
 
 
 
-// ------- RESTFUL API'S --------
+// ------- RESTful API'S --------
+// REST = Representational State Transfer
+// It is a type of API that requires client/server separation where communication only happens through url/data
+
+// Design Principles
+
+// 1. Client Server Decoupling
+// We want the front and back ends to be separated so they can be developed and scaled independantly,
+// using the 5 HTTP requests to do so.
+// GET | POST | DELETE | PUT | PATCH
+
+// 2. Statelessness
+// The requests must incorporate all data required by the server to perform its task, so it does not have to store,
+// any information (state). The server should never need to hold information for future requests.  
+
+// 3. Cacheability
+// Both the client and server can maintain their own cache. Servers can hold commonly accessed data by
+// multiple clients in cache, and clients can do the same for their own frequently recieved responses.
+
+// 4. Uniform Interface
+// Each client gets the same interface as well as the same data from the server. The same request for each client,
+// will use the same url, and give the same result.
+
+// 5. Layered System Architecture
+// A layered architecture allows neither the server or client to directly know if they are communicating with the 
+// other end or intermediaries, ensuring that seperation.
+
+// Anatomy of API endpoint
+// Endpoint: GET https://apiurl.com/posts/author/?limit=10&offset=20
+// GET | HTTP Request Verb Type
+// https://apiurl.com/ | Domain
+// /posts/ | Route
+// /author/ | Parameters (categories)
+// /?limit=10&offset=20 | Query (filters)
+// Hidden Part:
+// body: {"author": "John"} // Data sent when required, is json format
+
+// We can use Node.JS packages to locally test the REST API
+// The below options are the mosy popular:
+// - HttpYak (used in this course)
+// - Postman
+
 
 
 
